54deb641a205bb545fd6976b41db11aaca8a23d2
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2016-06-24 15:54:50 +0200

    add PageController.unauthorised action

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,10 @@ defmodule PrNotifier.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def unauthorized(conn, _params) do
+    conn
+      |> put_flash(:alert, "You need to login first!")
+      |> render("index.html")
+  end
 end
