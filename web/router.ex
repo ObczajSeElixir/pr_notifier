f0d44e8ef33515bde41b3e0ba265e1b8621b82e3
Mateusz Stebnicki <mateusz.stebnicki@netguru.pl>
2016-06-24 15:55:11 +0200

    add /auth route scope

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,11 @@ defmodule PrNotifier.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :assign_current_user
+  end
+
+  pipeline :authorized do
+    plug :authorize_user
   end
 
   pipeline :api do
@@ -14,15 +19,41 @@ defmodule PrNotifier.Router do
   end
 
   scope "/", PrNotifier do
-    pipe_through :browser # Use the default browser stack
+    pipe_through [:browser, :authorized]
 
     get "/", PageController, :index
     resources "/projects", ProjectController
     resources "/repositories", RepositoryController, only: [:index]
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", PrNotifier do
-  #   pipe_through :api
-  # end
+  scope "/", PrNotifier do
+    pipe_through :browser
+    get "/unauthorized", PageController, :unauthorized
+  end
+
+  scope "/auth", PrNotifier do
+    pipe_through :browser
+
+    get "/:provider", AuthController, :index
+    get "/:provider/callback", AuthController, :callback
+    delete "/logout", AuthController, :delete
+  end
+
+  # Fetch the current user from the session and add it to `conn.assigns`. This
+  # will allow you to have access to the current user in your views with
+  # `@current_user`.
+  defp assign_current_user(conn, _) do
+    assign(conn, :current_user, get_session(conn, :current_user))
+  end
+
+  defp authorize_user(conn, params) do
+    case get_session(conn, :current_user) do
+      nil ->
+        conn
+          |> redirect(to: "/unauthorized")
+          |> halt
+      _ ->
+        conn
+    end
+  end
 end
